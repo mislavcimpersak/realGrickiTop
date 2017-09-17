079d1709767bbac612e7f8116751682adc682951
Mislav Cimperšak <mislav.cimpersak@styria.hr>
2017-09-18 00:29:00 +0200

    get_wait_time test when DEBUG is on

--- tests/test_function.py
+++ tests/test_function.py
@@ -17,3 +17,17 @@ def test_poster__set_twitter_api():
 def test_poster__set_last_n_tweets():
     poster = Poster()
     assert len(poster.last_n_tweets) == 9
+
+
+@tape.use_cassette('test_poster.json')
+def test_poster__get_wait_time__with_debug():
+    os.environ['DEBUG'] = 'True'
+
+    poster = Poster()
+    tweet = {
+        "text": "BOOM!",
+        "time_span": "before"
+    }
+
+    wait_time = poster.get_wait_time(tweet)
+    assert wait_time == 0
