654f8f27fcf3da9e01246da8af5846f13d98ee63
Mislav Cimperšak <mislav.cimpersak@styria.hr>
2017-10-23 20:21:37 +0200

    single dummy test to setup ci

--- tests/test_function.py
+++ tests/test_function.py
@@ -7,27 +7,31 @@ from main import Poster
 from .config import tape
 
 
-@tape.use_cassette('test_poster.json')
-def test_poster__set_twitter_api():
-    poster = Poster()
-    assert poster.api.me().screen_name == 'realGrickiTop'
+def test_dummy():
+    assert 1 + 1 == 2
 
 
-@tape.use_cassette('test_poster.json')
-def test_poster__set_last_n_tweets():
-    poster = Poster()
-    assert len(poster.last_n_tweets) == 9
+# @tape.use_cassette('test_poster.json')
+# def test_poster__set_twitter_api():
+#     poster = Poster()
+#     assert poster.api.me().screen_name == 'realGrickiTop'
 
 
-@tape.use_cassette('test_poster.json')
-def test_poster__get_wait_time__with_debug():
-    os.environ['DEBUG'] = 'True'
+# @tape.use_cassette('test_poster.json')
+# def test_poster__set_last_n_tweets():
+#     poster = Poster()
+#     assert len(poster.last_n_tweets) == 9
 
-    poster = Poster()
-    tweet = {
-        "text": "BOOM!",
-        "time_span": "before"
-    }
 
-    wait_time = poster.get_wait_time(tweet)
-    assert wait_time == 0
+# @tape.use_cassette('test_poster.json')
+# def test_poster__get_wait_time__with_debug():
+#     os.environ['DEBUG'] = 'True'
+
+#     poster = Poster()
+#     tweet = {
+#         "text": "BOOM!",
+#         "time_span": "before"
+#     }
+
+#     wait_time = poster.get_wait_time(tweet)
+#     assert wait_time == 0
